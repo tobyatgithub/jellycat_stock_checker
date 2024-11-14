b293b169369bbbd65fffd7b00101d09fc317bdaf
tobyatgithub <toby.fangyuan@gmail.com>
2024-11-13 16:24:57 -0800

    web wechat api no longer working. pending for company version api.

---
+++ wechat_notification_text.py
@@ -0,0 +1,17 @@
+# # method 1 itchat ## nope, itchat is no longer maintained and outdated
+# import itchat
+
+# itchat.auto_login(hotReload=True)
+
+# itchat.send_msg(u'Hello, world!', toUserName='filehelper')
+
+# # method 2 wxpy ## -- same issue as itchat
+# from wxpy import Bot
+
+# # Initialize the bot and show QR code
+# bot = Bot(cache_path=True)
+
+# # Send message to file helper (testing purposes)
+# bot.file_helper.send('Hello, World!')
+
+# method 3
